942528d9a92ccedf8f5b4ceb89419cc336b56dbc
Renato Gonçalves de Lima <renato.glim@gmail.com>
2022-12-12 12:45:23 -0300

    Feature: modification 1day

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require("express") /*Serve para importar o express */
+const path = require("path") /*Lida com leitura de caminhos dos arquivos */
+
+let initialPath = path.join(__dirname, "public") /*Ele vai pegar a pasta inicial e j  */
